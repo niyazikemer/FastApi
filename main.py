883ce58d407f7800590247f4aaff4b11acf3b1e5
niyazikemer <nkemer@gmail.com>
2025-02-10 14:31:17 +0300

    how the client data handled on the server side.

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+#  http://127.0.0.1:8000/
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+#  http://127.0.0.1:8000/items/3
